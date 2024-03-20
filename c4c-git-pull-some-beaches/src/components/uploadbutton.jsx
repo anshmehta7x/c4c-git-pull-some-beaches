2fbe6f55b73a25f22fbcefc61c810e8410f15ad9
anshmehta7x <anshkapilmehta@gmail.com>
2024-03-20 19:57:22 +0530

    file uploadbutton

--- c4c-git-pull-some-beaches/src/components/uploadbutton.jsx
+++ c4c-git-pull-some-beaches/src/components/uploadbutton.jsx
@@ -1,8 +1,38 @@
+import React, { useRef } from 'react';
+
+
 export default function UploadButton({ type }) {
+
+    const fileInputRef = useRef(null);
+
+    const handleDrop = (event) => {
+        console.log(event)
+    }
+
+  const handleImageSelect = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const selectedImage = files[0];
+      console.log(selectedImage)
+    }
+  };
+
     return (
-        <div className="flex flex-col p-20 rounded-2xl drop-shadow-xl align-middle justify-center text-center">
+        <div onDrop={handleDrop} className="flex flex-col p-20 rounded-2xl drop-shadow-xl align-middle justify-center text-center">
             <div>
-                <button className="bg-new-blue w-[60vw] md:w-[20vw] text-white rounded-full px-5 py-3 font-bold">
+                <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+                />
+
+                <button onClick={handleImageSelect} className="bg-new-blue w-[60vw] md:w-[20vw] text-white rounded-full px-5 py-3 font-bold">
                     Upload {type}
                 </button>
             </div>
